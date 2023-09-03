9d996d5b74125238d44aef4ddc60487ed83b3cf4
thdud__ <love05061114@gmail.com>
2023-09-03 20:09:35 +0900

    타이틀화면 출력

--- main.cpp
+++ main.cpp
@@ -7,22 +7,35 @@ using namespace std;
 using namespace sf;
 
 struct Textures {
+	Texture title;
+	Texture btnStart;
+	Texture btnStart_sel;
 	Texture runner_def;
+	Texture runner_run;
 };
 
 class Runner {
 public:
 	Sprite sprite;
 
-	Runner(int speed) : speed_(speed) {
+	Runner(int speed, float run_sit) : speed_(speed), run_sit_(run_sit) {
 
+	}
+
+	float getRunSit(void) {
+		return run_sit_;
+	}
+
+	void setRunSit(float run_sit) {
+		run_sit = run_sit_;
 	}
 
 private:
 	int speed_;
-
+	float run_sit_;
 };
 
+int layer = 0;
 const int gravity = 20;
 
 const int WIDTH = 1000;
@@ -34,13 +47,20 @@ int main(void) {
 	RenderWindow window(VideoMode(WIDTH, HEIGHT), "Project");
 	window.setFramerateLimit(60);
 	Clock clock;
+	Clock walk_timer;
+
+	const Time walk_time = seconds(0.1f);
 
 	//텍스쳐
 	Textures tex;
-	tex.runner_def.loadFromFile("./resource/runner_def.png");
+	tex.title.loadFromFile("./resource/background/title.png");
+	tex.btnStart.loadFromFile("./resource/button/btnStart.png");
+	tex.btnStart_sel.loadFromFile("./resource/button/btnStart_sel.png");
+	tex.runner_def.loadFromFile("./resource/character/runner_def.png");
+	tex.runner_run.loadFromFile("./resource/character/runner_run.png");
 	
 
-	Runner runner = Runner(40);
+	Runner runner = Runner(40, 1);
 	runner.sprite.setTexture(tex.runner_def);
 	runner.sprite.setPosition(START_X, START_Y);
 	
@@ -56,14 +76,47 @@ int main(void) {
 				window.close();
 		}
 
-		//점프
-		if (Keyboard::isKeyPressed(Keyboard::Space)) {
+		//시작화면
+		Sprite title;
+		title.setTexture(tex.title);
+		title.setPosition(0, 0);
 
+		Sprite btnStart;
+		btnStart.setTexture(tex.btnStart);
+		btnStart.setPosition(WIDTH / 2 - 125, HEIGHT / 2 + 20);
+
+
+		//달리는 모습
+		while (layer == 1) {
+			if (walk_timer.getElapsedTime() > walk_time && runner.getRunSit() == 1) {
+				runner.sprite.setTexture(tex.runner_run);
+				runner.setRunSit(2);
+
+				walk_timer.restart();
+			}
+			if (walk_timer.getElapsedTime() > walk_time && runner.getRunSit() == 2) {
+				runner.sprite.setTexture(tex.runner_def);
+				runner.setRunSit(1);
+
+				walk_timer.restart();
+			}
+
+			//점프
+			if (Keyboard::isKeyPressed(Keyboard::Space)) {
+
+			}
 		}
 
-		window.clear(Color::White);
+		window.clear();
 
-		window.draw(runner.sprite);
+		if (layer == 0) {
+			window.draw(title);
+			window.draw(btnStart);
+		}
+
+		if (layer == 1) {
+			window.draw(runner.sprite);
+		}
 
 		window.display();
 	}
